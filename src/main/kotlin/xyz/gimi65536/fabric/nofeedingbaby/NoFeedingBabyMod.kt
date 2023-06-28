90292b317e7e1929a560abfb98b99bb81b56a231
gimi65536 <gimi65536@hotmail.com>
2023-06-28 17:27:14 +0900

    Test code to prevent cow babies from eating wheats TODO: How to extend codes to apply to all the baby animals? Is there an efficient way to filter foods for each type of animals?

--- src/main/kotlin/xyz/gimi65536/fabric/nofeedingbaby/NoFeedingBabyMod.kt
+++ src/main/kotlin/xyz/gimi65536/fabric/nofeedingbaby/NoFeedingBabyMod.kt
@@ -2,6 +2,10 @@ package xyz.gimi65536.fabric.nofeedingbaby
 
 import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
+import net.minecraft.util.ActionResult;
+import net.minecraft.entity.passive.CowEntity
+import net.minecraft.item.Items
+import net.fabricmc.fabric.api.event.player.UseEntityCallback
 
 object NoFeedingBabyMod : ModInitializer {
     private val logger = LoggerFactory.getLogger("no-feeding-baby")
@@ -10,6 +14,15 @@ object NoFeedingBabyMod : ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
+		UseEntityCallback.EVENT.register(fun(player, world, hand, entity, hitResult): ActionResult {
+			if(player.isSpectator()){
+				return ActionResult.PASS
+			}
+			if(entity is CowEntity && entity.isBaby() && player.getStackInHand(hand).item.equals(Items.WHEAT)){
+				return ActionResult.FAIL
+			}
+			return ActionResult.PASS
+		})
+		logger.info("Cow babies cannot eat wheat now.")
 	}
 }
