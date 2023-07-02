70edc022faa705dc368c5d632066e70a307b893f
gimi65536 <gimi65536@hotmail.com>
2023-07-02 23:46:46 +0900

    Update

--- src/main/kotlin/xyz/gimi65536/fabric/nofeedingbaby/config/NoFeedingBabyYACLConfig.kt
+++ src/main/kotlin/xyz/gimi65536/fabric/nofeedingbaby/config/NoFeedingBabyYACLConfig.kt
@@ -1,11 +1,5 @@
 package xyz.gimi65536.fabric.nofeedingbaby.config
 
-import com.electronwill.nightconfig.core.Config
-import com.electronwill.nightconfig.core.file.FileConfig
-import java.io.File
-import kotlin.collections.LinkedHashSet
-import kotlin.io.path.exists
-import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.client.gui.screen.Screen
 
 class NoFeedingBabyYACLConfig: NoFeedingBabyConfig() {
