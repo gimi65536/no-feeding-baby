612361b2c48ff5a2d5fe19fe188f8f24288cef65
gimi65536 <gimi65536@hotmail.com>
2023-07-04 20:10:13 +0900

    Fill in translatable

--- src/client/kotlin/xyz/gimi65536/fabric/nofeedingbaby/config/YACLIntegration.kt
+++ src/client/kotlin/xyz/gimi65536/fabric/nofeedingbaby/config/YACLIntegration.kt
@@ -9,54 +9,46 @@ import dev.isxander.yacl3.api.YetAnotherConfigLib
 import dev.isxander.yacl3.gui.controllers.BooleanController
 import dev.isxander.yacl3.gui.controllers.string.StringController
 import net.minecraft.client.gui.screen.Screen
-import net.minecraft.text.MutableText
 import net.minecraft.text.Style
 import net.minecraft.text.Text
-import net.minecraft.text.TextContent
 import xyz.gimi65536.fabric.nofeedingbaby.NoFeedingBabyMod
 import xyz.gimi65536.fabric.nofeedingbaby.config.NoFeedingBabyConfig
 
 object YACLIntegration{
 	fun MODE_LIST_FORMATTER(whitelistMode: Boolean): Text {
 		return when(whitelistMode){
-			true -> Text.literal("Whitelist")
-			false -> Text.literal("Blacklist")
+			true -> Text.translatable("no-feeding-baby.whitelist")
+			false -> Text.translatable("no-feeding-baby.blacklist")
 		}
 	}
-	private val WHITELIST_TEXT: Text = MutableText.of(TextContent.EMPTY)
-		.append("Whitelist")
-		.setStyle(Style.EMPTY
-			.withColor(0xffffff)
-			.withBold(true)
-			.withUnderline(true)
-		)
-	private val BLACKLIST_TEXT: Text = MutableText.of(TextContent.EMPTY)
-		.append("Blacklist")
-		.setStyle(Style.EMPTY
-			.withColor(0x222222)
-			.withBold(true)
-			.withUnderline(true)
-		)
 	fun MODE_DESCRIPTION(whitelistMode: Boolean): OptionDescription {
 		return when (whitelistMode) {
 			true -> OptionDescription.createBuilder()
-				.text(MutableText.of(TextContent.EMPTY).append(WHITELIST_TEXT).append(" mode"))
-				.text(Text.literal("ONLY baby animals listed CAN be fed"))
+				.text(Text.translatable("no-feeding-baby.whitelistmode").setStyle(Style.EMPTY
+					.withColor(0xffffff)
+					.withBold(true)
+					.withUnderline(true)
+				))
+				.text(Text.translatable("no-feeding-baby.whitelist-tooltip"))
 				.build()
 			false -> OptionDescription.createBuilder()
-				.text(MutableText.of(TextContent.EMPTY).append(BLACKLIST_TEXT).append(" mode"))
-				.text(Text.literal("ONLY baby animals listed CANNOT be fed"))
+				.text(Text.translatable("no-feeding-baby.blacklistmode").setStyle(Style.EMPTY
+					.withColor(0x666666)
+					.withBold(true)
+					.withUnderline(true)
+				))
+				.text(Text.translatable("no-feeding-baby.blacklist-tooltip"))
 				.build()
 		}
 	}
 
 	fun getModConfigScreen(parent: Screen): Screen {
 		return YetAnotherConfigLib.createBuilder()
-			.title(Text.literal("No Feeding Baby Configuration"))
+			.title(Text.translatable("no-feeding-baby.configtitle"))
 			.category(ConfigCategory.createBuilder()
-				.name(Text.literal("General"))
+				.name(Text.translatable("no-feeding-baby.general"))
 				.option(Option.createBuilder<Boolean>()
-					.name(Text.literal("Mode"))
+					.name(Text.translatable("no-feeding-baby.mode"))
 					.description(::MODE_DESCRIPTION)
 					.binding(
 						NoFeedingBabyConfig.DEFAULT_WHITELISTMODE,
@@ -67,7 +59,7 @@ object YACLIntegration{
 					.build()
 				)
 				.group(ListOption.createBuilder<String>()
-					.name(Text.literal("Animal")) // Since the ListOptionEntry shows the group name redundantly
+					.name(Text.translatable("no-feeding-baby.animal")) // Since the ListOptionEntry shows the group name redundantly
 					.collapsed(false)
 					.binding(
 						listOf(),
