ebc7ec42b3161034c0f499076cc20e6094d6c5bb
gimi65536 <gimi65536@hotmail.com>
2023-07-05 21:21:09 +0900

    Show message when toggling

--- src/client/kotlin/xyz/gimi65536/fabric/nofeedingbaby/NoFeedingBabyModClient.kt
+++ src/client/kotlin/xyz/gimi65536/fabric/nofeedingbaby/NoFeedingBabyModClient.kt
@@ -3,9 +3,12 @@ package xyz.gimi65536.fabric.nofeedingbaby
 import net.fabricmc.api.ClientModInitializer
 import net.minecraft.client.option.KeyBinding
 import net.minecraft.client.util.InputUtil
+import net.minecraft.client.MinecraftClient
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.passive.AnimalEntity
 import net.minecraft.util.ActionResult
+import net.minecraft.text.Text
+import net.minecraft.text.Style
 import net.fabricmc.fabric.api.event.player.UseEntityCallback
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import xyz.gimi65536.fabric.nofeedingbaby.config.NoFeedingBabyConfig
@@ -33,18 +36,49 @@ object NoFeedingBabyModClient : ClientModInitializer {
 				if(NoFeedingBabyMod.checkValid(player, hand, entity)){
 					if(toggleModifier.isPressed()){
 						// Do toggling
-						val identifier = EntityType.getId(entity.getType()).toString()
-						val toggleResult = NoFeedingBabyConfig.toggleAnimal(identifier)
+						val identifier = EntityType.getId(entity.getType())
+						val toggleResult = NoFeedingBabyConfig.toggleAnimal(identifier.toString())
+						val entityTranskey = "entity-%s-%s".format(identifier.namespace, identifier.path)
 						if(toggleResult){
 							// Show something
+							MinecraftClient.getInstance().inGameHud.setOverlayMessage(
+								Text.translatable("no-feeding-baby.added-1")
+								.append(Text.translatable(entityTranskey).setStyle(Style.EMPTY.withBold(true)))
+								.append(Text.translatable("no-feeding-baby.added-2"))
+								.append(
+									if(NoFeedingBabyConfig.whitelistMode){
+										Text.translatable("no-feeding-baby.whitelist")
+										.setStyle(NoFeedingBabyMod.WHITELIST_STYLE)
+									}else{
+										Text.translatable("no-feeding-baby.blacklist")
+										.setStyle(NoFeedingBabyMod.BLACKLIST_STYLE)
+									}
+								)
+								.append(Text.translatable("no-feeding-baby.added-3"))
+							, true)
 						}else{
 							// Show something
+							MinecraftClient.getInstance().inGameHud.setOverlayMessage(
+								Text.translatable("no-feeding-baby.removed-1")
+								.append(Text.translatable(entityTranskey).setStyle(Style.EMPTY.withBold(true)))
+								.append(Text.translatable("no-feeding-baby.removed-2"))
+								.append(
+									if(NoFeedingBabyConfig.whitelistMode){
+										Text.translatable("no-feeding-baby.whitelist")
+										.setStyle(NoFeedingBabyMod.WHITELIST_STYLE)
+									}else{
+										Text.translatable("no-feeding-baby.blacklist")
+										.setStyle(NoFeedingBabyMod.BLACKLIST_STYLE)
+									}
+								)
+								.append(Text.translatable("no-feeding-baby.removed-3"))
+							, true)
 						}
 						try {
 							NoFeedingBabyConfig.save()
 						}
 						catch(Exception e) {
-							// Show something
+							// Silent
 						}
 						// Do not send feeding packet
 						return ActionResult.FAIL
